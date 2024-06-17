58819e9ddad6a151ef7bb4bfff5fb8e1172f596c
Gabriel Ivanov <gabunivanov@gmail.com>
2024-06-17 17:21:02 +0300

    Added entities

---
+++ EventManagement.Data/Entities/Feedback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Data.Entities
+{
+    public class Feedback : BaseEntity
+    {
+        public int FeedbackId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ EventManagement.Data/Entities/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagement.Data.Entities
+{
+   public class Ticket : BaseEntity
+    {
+        public int TicketId { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
+    }
+}
